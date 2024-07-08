412d7578705e79981f401bf4b0f60f42a68fc8c5
Ezra Kirsh <ezkirsh@gmail.com>
2024-07-08 12:13:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.get('/instagram', async (req, res) => {
     const filter = req.query.filter || '';
     const filters = filter.split(',').filter(f => f);
     console.log(filters);
-    const query = filters.length ? { 'following_list.username': { $in: filters } } : {};
-    
+    const query = filters.length ? { 'follower_list.username': { $in: filters } } : {};
+
     const skip = (page - 1) * limit;
 
     let sortQuery = {};
