4183aa8dfe8b2f0e2d32345789b5744797920656
Ezra Kirsh <ezkirsh@gmail.com>
2024-07-15 10:42:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/instagram', async (req, res) => {
     const db = client.db('instagram_data');
     const collection = db.collection('users');
 
-    const users = await collection.find()
+    const users = await collection.find(query)
       .sort(sortQuery)
       .skip(skip)
       .limit(limit)
